515da251f20be4d7dc9c5258f5da401dbf294975
dancg <dannycalgon@gmail.com>
2018-01-29 14:24:21 +0100

    Cambios configurados y listo para empezar

--- provision/nginx.sh
+++ provision/nginx.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#Obtener lista de paquetes
+# Retrieve new lists of packages
 apt-get update
 
-#Instalamos nginx
+# Install nginx
 apt-get install -y nginx
 
-#Instalamos los paquetes de php para nginx
+# Install php packages for nginx
 apt-get install -y php-fpm php-mysql php-curl php-gd php-mbstring php-mcrypt php-xml php-xmlrpc
 
-#Configurar php para nginx
-sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/' /etc/php/7.0/fpm/php.ini
+# Configure php for nginx
+sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.0/fpm/php.ini
 systemctl restart php7.0-fpm
 
-#Copiar fichero de configuracion para nginx
+# Copy configuration file for nginx
 cp /vagrant/config/nginx.default /etc/nginx/sites-available/default
-/etc/init.d/nginx restart
+systemctl reload nginx
