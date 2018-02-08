1c0aa6d611350a6aa3477bd3cfa48ee690b5ee79
dancg <dannycalgon@gmail.com>
2018-02-08 09:33:18 +0100

    Subiendo el ejercicio-10

---
+++ projects/ejercicio-10.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lanzar una moneda</title>
+</head>
+<body>
+    <?php
+      $moneda = rand(0,1);
+      if ($moneda == 0){
+          echo "<img src=\"images/cara.jpeg\" />";
+      }else{
+          echo "<img src=\"images/cruz.jpeg\" />";
+      }
+
+
+    ?>
+</body>
+</html>
