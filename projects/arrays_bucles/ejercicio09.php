d9e65550df76a8ac868da86860f19cc08fcce3df
dancg <dannycalgon@gmail.com>
2018-03-12 11:14:01 +0100

    Subida de los ejercicios 09 y 10 de arrays_bucles

--- projects/arrays_bucles/ejercicio09.php
+++ projects/arrays_bucles/ejercicio09.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE <html>
 <html>
 <head>
@@ -6,12 +7,15 @@
 </head> 
     <body>
 
+
     <?php
     $lugares = array("Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", "Sweden"=>"Stockholm", "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague", "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga", "Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw");
-    echo "<pre>";
-    echo json_encode($lugares);
-    echo "</pre>";
+    $var_contador = count($lugares);
+    ksort($lugares);
+    foreach ($lugares as $clave => $valor) {
+        echo " La capital de $clave es $valor <br/><br/>";
+    }
     ?>
 
     </body> 
-</html> 
+</html>
